972c98b2fccccdaea48a11090e44ca6d264f0b69
Ricardo Koller <rkoll001@fiu.edu>
2016-02-05 13:22:10 -0500

    Update mem.c
    nits

--- kernel/mem.c
+++ kernel/mem.c
@@ -1,5 +1,5 @@
-/* Copyright (c) 2015, IBM
- * Author(s): Dan Williams <djwillia@us.ibm.com>
+/* Copyright (c) 2015, IBM 
+ * Author(s): Dan Williams <djwillia@us.ibm.com> 
  *
  * Permission to use, copy, modify, and/or distribute this software
  * for any purpose with or without fee is hereby granted, provided
