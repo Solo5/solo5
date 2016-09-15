9143b4c69e60187bc656c4e66d2492a97d5323d3
Martin Lucina <martin.lucina@docker.com>
2016-09-15 17:55:12 +0200

    Cosmetics

--- kernel/virtio/platform_intr.c
+++ kernel/virtio/platform_intr.c
@@ -18,30 +18,28 @@
 
 #include "kernel.h"
 
-#define PIC1 0x20   /* IO base address for master PIC */
-#define PIC2 0xA0   /* IO base address for slave PIC */
-#define PIC1_COMMAND PIC1
-#define PIC1_DATA (PIC1 + 1)
-#define PIC2_COMMAND PIC2
-#define PIC2_DATA (PIC2 + 1)
+#define PIC1 0x20        /* IO base address for master PIC */
+#define PIC2 0xA0        /* IO base address for slave PIC */
+#define PIC1_COMMAND     PIC1
+#define PIC1_DATA        (PIC1 + 1)
+#define PIC2_COMMAND     PIC2
+#define PIC2_DATA        (PIC2 + 1)
 #define IRQ_ON_MASTER(n) ((n) < 8)
 #define IRQ_PORT(n)      (IRQ_ON_MASTER(n) ? PIC1_DATA : PIC2_DATA)
 #define IRQ_OFFSET(n)    (IRQ_ON_MASTER(n) ? (n) : ((n) - 8))
 
-#define INTR_IRQ_MASTER INTR_IRQ_0
-#define INTR_IRQ_SLAVE  INTR_IRQ_8
+#define PIC_EOI         0x20 /* End-of-interrupt command code */
+#define ICW1_ICW4       0x01 /* ICW4 (not) needed */
+#define ICW1_SINGLE     0x02 /* Single (cascade) mode */
+#define ICW1_INTERVAL   0x04 /* Call address interval 4 (8) */
+#define ICW1_LEVEL      0x08 /* Level triggered (edge) mode */
+#define ICW1_INIT       0x10 /* Initialization - required! */
+#define ICW4_8086       0x01 /* 8086/88 (MCS-80/85) mode */
+#define ICW4_AUTO       0x02 /* Auto (normal) EOI */
+#define ICW4_BUF_SLAVE  0x08 /* Buffered mode/slave */
+#define ICW4_BUF_MASTER 0x0C /* Buffered mode/master */
+#define ICW4_SFN        0x10 /* Special fully nested (not) */
 
-#define PIC_EOI 0x20/* End-of-interrupt command code */
-#define ICW1_ICW4 0x01/* ICW4 (not) needed */
-#define ICW1_SINGLE 0x02/* Single (cascade) mode */
-#define ICW1_INTERVAL 0x04/* Call address interval 4 (8) */
-#define ICW1_LEVEL 0x08/* Level triggered (edge) mode */
-#define ICW1_INIT 0x10/* Initialization - required! */
-#define ICW4_8086 0x01/* 8086/88 (MCS-80/85) mode */
-#define ICW4_AUTO 0x02/* Auto (normal) EOI */
-#define ICW4_BUF_SLAVE 0x08/* Buffered mode/slave */
-#define ICW4_BUF_MASTER 0x0C/* Buffered mode/master */
-#define ICW4_SFN M0x10/* Special fully nested (not) */
 #define io_wait() do { } while (0)
 
 /*
