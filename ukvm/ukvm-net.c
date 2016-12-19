78b134fd00fa724c5aeb4710cfd93b137a5deb02
Martin Lucina <martin.lucina@docker.com>
2016-12-19 17:00:50 +0100

    Fix error handling for --net-mac

--- ukvm/ukvm-net.c
+++ ukvm/ukvm-net.c
@@ -188,8 +188,10 @@ static int handle_cmdarg(char *cmdarg)
         if (sscanf(macptr,
                    "%02"SCNx8":%02"SCNx8":%02"SCNx8":"
                    "%02"SCNx8":%02"SCNx8":%02"SCNx8,
-                   &mac[0], &mac[1], &mac[2], &mac[3], &mac[4], &mac[5]) != 6)
-            err(1, "Malformed mac address: %s", macptr);
+                   &mac[0], &mac[1], &mac[2], &mac[3], &mac[4], &mac[5]) != 6) {
+            warnx("Malformed mac address: %s", macptr);
+            return -1;
+        }
         snprintf(netinfo.mac_str, sizeof(netinfo.mac_str), "%s", macptr);
         cmdline_mac = true;
         return 0;
