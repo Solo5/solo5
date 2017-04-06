4d61030ba671f72104ef7edcc053db0021ddc312
Martin Lucina <martin.lucina@docker.com>
2017-04-06 12:29:57 +0200

    Add build.sh script for surf-build

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# THIS IS NOT THE BUILD SCRIPT FOR Solo5.
+#
+# This script is used for automated builds with surf-build.
+# See https://github.com/Solo5/solo5-ci for details.
+#
+if type gmake >/dev/null 2>&1; then
+    MAKE=gmake
+else
+    MAKE=make
+fi
+
+set -xe
+${MAKE}
+# Don't run any tests yet
