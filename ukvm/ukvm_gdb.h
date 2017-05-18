e58decfb669888f43fcf2da59fad3915a7b88be1
Ricardo Koller <kollerr@us.ibm.com>
2017-05-18 14:12:54 -0400

    gdb: cleanup
    1. Removed all arch and backend dependent code from ukvm/ukvm_module_gdb.c
    2. Cleaned up ukvm/ukvm_module_gdb.c, fixed issue #25.
    3. Implemented some new commands like: writing memory and registers.
    4. Added a stub freebsd ukvm_gdb_freebsd_x86_64.c
    5. Breakpoints without relying on stepping. Issue #16.

---
+++ ukvm/ukvm_gdb.h
@@ -0,0 +1,40 @@
+/* 
+ * Copyright (c) 2015-2017 Contributors as noted in the AUTHORS file
+ *
+ * This file is part of ukvm, a unikernel monitor.
+ *
+ * Permission to use, copy, modify, and/or distribute this software
+ * for any purpose with or without fee is hereby granted, provided
+ * that the above copyright notice and this permission notice appear
+ * in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+ * WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+ * AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR
+ * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+ * NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifndef UKVM_GDB_H
+#define UKVM_GDB_H
+
+/* GDB breakpoint/watchpoint types */
+#define GDB_BREAKPOINT_SW        0
+#define GDB_BREAKPOINT_HW        1
+#define GDB_WATCHPOINT_WRITE     2
+#define GDB_WATCHPOINT_READ      3
+#define GDB_WATCHPOINT_ACCESS    4
+
+#define GDB_SIGNAL_FIRST         0
+#define GDB_SIGNAL_QUIT          3
+#define GDB_SIGNAL_KILL          9
+#define GDB_SIGNAL_TRAP          5
+#define GDB_SIGNAL_SEGV          11
+#define GDB_SIGNAL_TERM          15
+#define GDB_SIGNAL_IO            23
+#define GDB_SIGNAL_DEFAULT       144
+
+#endif /* #ifndef UKVM_GDB_H */
