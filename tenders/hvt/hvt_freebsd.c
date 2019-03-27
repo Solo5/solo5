1bcf9a4647c34b9cf04f58d2bbc752471def6d3f
Martin Lucina <martin@lucina.net>
2019-03-27 12:31:31 +0100

    hvt: FreeBSD: Revert drop privileges for now

--- tenders/hvt/hvt_freebsd.c
+++ tenders/hvt/hvt_freebsd.c
@@ -135,21 +135,8 @@ struct hvt *hvt_init(size_t mem_size)
 #if HVT_DROP_PRIVILEGES
 void hvt_drop_privileges()
 {
-    struct passwd *pw = getpwnam(VMM_USER);
-    if (pw == NULL)
-        err(1, "can't get %s user", VMM_USER);
-    uid_t uid = pw->pw_uid;
-    gid_t gid = pw->pw_gid;
-
-    if (chroot(VMM_CHROOT) == -1)
-        err(1, "chroot(%s)", VMM_CHROOT);
-
-    if (chdir("/") == -1)
-        err(1, "chdir(/)");
-
-    if (setgroups(1, &gid) ||
-            setresgid(gid, gid, gid) ||
-            setresuid(uid, uid, uid))
-        err(1, "unable to revoke privs");
+    /*
+     * This function intentionally left blank for now (see #282).
+     */
 }
 #endif
