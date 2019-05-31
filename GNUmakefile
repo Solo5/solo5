cc764edb1347735b3e1ed61ab955082aa88087b0
Martin Lucina <martin@lucina.net>
2019-05-31 15:43:46 +0200

    mft: Cleanup

--- GNUmakefile
+++ GNUmakefile
@@ -21,9 +21,9 @@ $(TOPDIR)/Makeconf:
 	$(error Makeconf not found, please run ./configure.sh)
 include Makefile.common
 
-SUBDIRS := bindings tenders manifest tests
+SUBDIRS := bindings tenders mfttool tests
 
-tests: bindings manifest
+tests: bindings mfttool
 
 .PHONY: $(SUBDIRS)
 
--- mfttool/mfttool.c
+++ mfttool/mfttool.c
@@ -1,3 +1,31 @@
+/*
+ * Copyright (c) 2015-2019 Contributors as noted in the AUTHORS file
+ *
+ * This file is part of Solo5, a sandboxed execution environment.
+ *
+ * Permission to use, copy, modify, and/or distribute this software
+ * for any purpose with or without fee is hereby granted, provided
+ * that the above copyright notice and this permission notice appear
+ * in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+ * WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+ * AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR
+ * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+ * NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/*
+ * mfttool.c: Manifest generator.
+ *
+ * This tool produces a binary manifest from its JSON source. The produced C
+ * source file should be compiled with the Solo5 toolchain and linked into the
+ * unikernel binary.
+ */
+
 #define _GNU_SOURCE
 #include <assert.h>
 #include <err.h>
@@ -28,7 +56,7 @@ static void jexpect(enum jtypes t, jvalue *v, const char *loc)
 
 static const char out_header[] = \
     "#define MFT_ENTRIES %d\n"
-    "#include \"mft_types.h\"\n"
+    "#include \"mft_abi.h\"\n"
     "\n"
     "MFT_NOTE_BEGIN\n"
     "{\n"
