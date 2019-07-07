c54142db3b6062a2333286a563f5cf90673a72d4
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2019-07-07 22:58:37 +0200

    muen: Adapt dummy block implementation to API changes
    Block devices are not (yet) supported on Muen, thus only update all
    block functions to their new signatures.

--- bindings/muen/muen-block.c
+++ bindings/muen/muen-block.c
@@ -18,23 +18,31 @@
  * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-#include "../bindings.h"
+#include "../hvt/bindings.h"
 
-solo5_result_t solo5_block_write(solo5_off_t offset __attribute__((unused)),
-                         const uint8_t *buf __attribute__((unused)),
-                         size_t size __attribute__((unused)))
+solo5_result_t solo5_block_acquire(const char *name __attribute__((unused)),
+        solo5_handle_t *handle __attribute__((unused)),
+        struct solo5_block_info *info __attribute__((unused)))
 {
     return SOLO5_R_EUNSPEC;
 }
 
-solo5_result_t solo5_block_read(solo5_off_t offset __attribute__((unused)),
-                        uint8_t *buf __attribute__((unused)),
-                        size_t size __attribute__((unused)))
+solo5_result_t solo5_block_write(solo5_handle_t handle __attribute__((unused)),
+        solo5_off_t offset __attribute__((unused)),
+        const uint8_t *buf __attribute__((unused)),
+        size_t size __attribute__((unused)))
 {
     return SOLO5_R_EUNSPEC;
 }
 
-void solo5_block_info(struct solo5_block_info *info __attribute__((unused)))
+solo5_result_t solo5_block_read(solo5_handle_t handle __attribute__((unused)),
+        solo5_off_t offset __attribute__((unused)),
+        uint8_t *buf __attribute__((unused)),
+        size_t size __attribute__((unused)))
 {
-    assert(0);
+    return SOLO5_R_EUNSPEC;
+}
+
+void block_init(struct hvt_boot_info *bi __attribute__((unused)))
+{
 }
