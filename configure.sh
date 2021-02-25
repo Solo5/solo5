95200a09b169ca52b40aa12882de3a68fe619747
Martin Lucina <martin@lucina.net>
2021-02-25 08:23:02 +0000

    Remove Genode bindings
    The Genode bindings have been unmaintained for some time, and are
    incompatible with the upcoming LLVM/Clang based toolchain. Remove them
    for now, they can be reinstated in the future if there is interest.
    
    Note that GENODE_ABI_TARGET is not removed, since once allocated the
    value should not be re-used by any future bindings.

--- configure.sh
+++ configure.sh
@@ -175,7 +175,6 @@ config_host_linux()
     [ "${CONFIG_ARCH}" = "x86_64" ] && CONFIG_VIRTIO=1
     [ "${CONFIG_ARCH}" = "x86_64" ] && CONFIG_MUEN=1
     [ "${CONFIG_ARCH}" = "ppc64le" ] && CONFIG_HVT=
-    CONFIG_GENODE=
     [ "${CONFIG_ARCH}" = "x86_64" ] && CONFIG_XEN=1
 }
 
@@ -214,7 +213,6 @@ config_host_freebsd()
     CONFIG_SPT=
     [ "${CONFIG_ARCH}" = "x86_64" ] && CONFIG_VIRTIO=1
     [ "${CONFIG_ARCH}" = "x86_64" ] && CONFIG_MUEN=1
-    CONFIG_GENODE=
     [ "${CONFIG_ARCH}" = "x86_64" ] && CONFIG_XEN=1
 }
 
@@ -247,7 +245,6 @@ config_host_openbsd()
     CONFIG_SPT=
     [ "${CONFIG_ARCH}" = "x86_64" ] && CONFIG_VIRTIO=1
     [ "${CONFIG_ARCH}" = "x86_64" ] && CONFIG_MUEN=1
-    CONFIG_GENODE=
     [ "${CONFIG_ARCH}" = "x86_64" ] && CONFIG_XEN=1
 }
 
@@ -304,7 +301,6 @@ CONFIG_HVT=
 CONFIG_SPT=
 CONFIG_VIRTIO=
 CONFIG_MUEN=
-CONFIG_GENODE=
 CONFIG_XEN=
 MAKECONF_CFLAGS=
 MAKECONF_LDFLAGS=
@@ -351,7 +347,6 @@ CONFIG_HVT=${CONFIG_HVT}
 CONFIG_SPT=${CONFIG_SPT}
 CONFIG_VIRTIO=${CONFIG_VIRTIO}
 CONFIG_MUEN=${CONFIG_MUEN}
-CONFIG_GENODE=${CONFIG_GENODE}
 CONFIG_XEN=${CONFIG_XEN}
 MAKECONF_CFLAGS=${MAKECONF_CFLAGS}
 MAKECONF_LDFLAGS=${MAKECONF_LDFLAGS}
@@ -372,6 +367,5 @@ echo -n "${prog_NAME}: Enabled targets:"
 [ -n "${CONFIG_SPT}" ]    && echo -n " spt"
 [ -n "${CONFIG_VIRTIO}" ] && echo -n " virtio"
 [ -n "${CONFIG_MUEN}" ]   && echo -n " muen"
-[ -n "${CONFIG_GENODE}" ] && echo -n " genode"
 [ -n "${CONFIG_XEN}" ]    && echo -n " xen"
 echo "."
