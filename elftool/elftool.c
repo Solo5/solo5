972c1613109b28e4bc9bb3aa834809f7ed21ea89
Hannes Mehnert <hannes@mehnert.org>
2026-09-23 23:07:38 +0200

    introduce valid_dev_name

--- elftool/elftool.c
+++ elftool/elftool.c
@@ -114,6 +114,15 @@ static void usage(const char *prog)
     exit(EXIT_FAILURE);
 }
 
+static void valid_dev_name(const char *data, const char *source, size_t line)
+{
+  if (strlen(data) > MFT_NAME_MAX)
+    errx(1, "%s:%zu name too long: %s", source, line, data);
+  for (const char *p = data; *p; p++)
+    if (!isalnum((unsigned char)*p))
+      errx(1, "%s:%zu name %s is not alphanumeric", source, line, data);
+}
+
 static int elftool_gen_mft(const char *source, const char *output)
 {
     FILE *sfp = strncmp(source, "-", 2) ? fopen(source, "r") : stdin;
@@ -198,13 +207,7 @@ static int elftool_gen_mft(const char *source, const char *output)
         if (jdevname->u.s[0] == 0)
             errx(1, "%s:%zu: .devices[...]: .name may not be empty", source,
                  jdevname->line);
-        if (strlen(jdevname->u.s) > MFT_NAME_MAX)
-            errx(1, "%s:%zu: .devices[...]: name too long", source,
-                 jdevname->line);
-        for (char *p = jdevname->u.s; *p; p++)
-            if (!isalnum((unsigned char)*p))
-                errx(1, "%s:%zu: .devices[...]: name is not alphanumeric",
-                     source, jdevname->line);
+        valid_dev_name(jdevname->u.s, source, jdevname->line);
         if (jdevtype == NULL)
             errx(1, "%s:%zu: .devices[...]: missing .type", source, (*i)->line);
         fprintf(ofp, out_entry, jdevname->u.s, jdevtype->u.s);
@@ -214,18 +217,6 @@ static int elftool_gen_mft(const char *source, const char *output)
     fclose(ofp);
     jdel(root);
     return EXIT_SUCCESS;
-}
-
-static bool valid_json_string(const char *data)
-{
-  for (uint i = 0; i < strlen(data); i++) {
-    if (data[i] != 0x2d && data[i] != 0x2e &&
-        !(data[i] >= 0x30 && data[i] <= 0x5a) &&
-        !(data[i] >= 0x61 && data[i] <= 0x7a)) {
-      return false;
-    }
-  }
-  return true;
 }
 
 static int elftool_query_mft(const char *binary)
@@ -260,10 +251,8 @@ static int elftool_query_mft(const char *binary)
     for (unsigned i = 0; i != mft->entries; i++) {
         if (mft->e[i].type >= MFT_RESERVED_FIRST)
             continue;
-        if (!valid_json_string(mft->e[i].name)) {
-          warnx("%s: Invalid device name", mft->e[i].name);
-          return EXIT_FAILURE;
-        }
+
+        valid_dev_name(mft->e[i].name, binary, 0);
         printf("    { \"name\": \"%s\", \"type\": \"%s\" }%s\n", mft->e[i].name,
                mft_type_to_string(mft->e[i].type),
                (i == (mft->entries - 1) ? "" : ","));
